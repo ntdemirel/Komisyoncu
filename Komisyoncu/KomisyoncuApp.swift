f2015f02a035726fe29d3206ac12f11cddb6fe15
ntdemirel <nafiztahademirel@gmail.com>
2026-01-13 13:28:35 +0300

    Setup project + Supabase SPM + secrets config

---
+++ Komisyoncu/KomisyoncuApp.swift
@@ -0,0 +1,17 @@
+//
+//  KomisyoncuApp.swift
+//  Komisyoncu
+//
+//  Created by Taha DEMİREL on 13.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct KomisyoncuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Komisyoncu/SupabaseClientProvider.swift
@@ -0,0 +1,34 @@
+//
+//  SupabaseClientProvider.swift
+//  Komisyoncu
+//
+//  Created by Taha DEMİREL on 13.01.2026.
+//
+
+import Foundation
+import Supabase
+
+enum AppConfig {
+    
+    static var supabaseURL : URL {
+        guard
+            let raw = Bundle.main.object(forInfoDictionaryKey: "SUPABASE_URL") as? String,
+            let url = URL(string: raw)
+        else {
+            fatalError("Missing SUPABASE_URL in Info.plist")
+        }
+        return url
+    }
+    
+    static var supabaseAnonKey: String {
+        
+        guard let key = Bundle.main.object(forInfoDictionaryKey: "SUPABASE_ANON_KEY") as? String else {
+            fatalError("Missing SUPABASE_ANON_KEY in Info.plist")
+        }
+        return key
+    }
+    
+    enum SupabaseProvider {
+        static let client = SupabaseClient(supabaseURL: AppConfig.supabaseURL, supabaseKey: AppConfig.supabaseAnonKey)
+    }
+}
